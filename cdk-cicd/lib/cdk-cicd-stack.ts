9d206ffe9a9fe684508b28046178e13f3df3a899
Deepak Masuti <deepakmasuti@Deepaks-MacBook-Air.local>
2025-04-07 01:17:04 +0530

    abcd

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -10,10 +10,9 @@ export class CdkCicdStack extends cdk.Stack {
       synth: new ShellStep('myShellStep', {
         input: CodePipelineSource.gitHub('deeep14/aws-cdk', 'cdk-cicd'),
         commands: [
+          'cd cdk-cicd',
           'npm ci',
-          'npx cdk synth',
-          'ls -la',
-          'ls -la cdk.out'
+          'npx cdk synth'
         ],
         primaryOutputDirectory: 'cdk.out'
       })
