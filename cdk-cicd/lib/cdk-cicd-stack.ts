6ff6a95cf53c4b9c3c665cbb5ddb56ce9ecc46ba
Deepak Masuti <deepakmasuti@Deepaks-MacBook-Air.local>
2025-04-08 14:02:05 +0530

    op-change

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -11,7 +11,7 @@ export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: pipelineProps) {
     super(scope, id, props);
     const pipeline = new CodePipeline(this, 'myCodePipelineAwsCdk', {
-      pipelineName: 'myCodePipelineAwsCdk',
+      pipelineName: 'cdk-code-pipeline',
       synth: new ShellStep('myShellStep', {
         input: CodePipelineSource.gitHub('deeep14/aws-cdk', 'cdk-cicd'),
         commands: [
@@ -19,7 +19,7 @@ export class CdkCicdStack extends cdk.Stack {
           'npm ci',
           'npx cdk synth'
         ],
-        primaryOutputDirectory: 'cdk.out'
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
       })
     })
     pipeline.addStage(new PipelineStage(this, 'TestStage', {
