fe5c3b72cb3e97d8e8fbabdac137d34acc24af90
Deepak <deepakmasuti101@gmail.com>
2025-07-11 14:23:19 +0530

    pushing code

--- cdk-cicd/lib/pipeline-stage.ts
+++ cdk-cicd/lib/pipeline-stage.ts
@@ -50,7 +50,6 @@ export class PipelineStage extends Stage {
         const s3ToSqsQueue = new S3ToSqsStack(this, props.S3ToSqsStack, {
             bucket: importedBucketInstance.importedBucket
         })
-        s3ToSqsQueue.addDependency(importedBucketInstance);
         const SqsToLambda = new SqsToLambdaStack(this, props.SqsToLambdaStack, {
             queue: s3ToSqsQueue.queue,
             lambdaFunction: lambdaInstace.test_lambda
